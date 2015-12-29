0cb23ace8fc0af0d04b86f4d32a589c2b1860cf1
Rick Patci <rpatci@gmail.com>
2015-12-29 11:29:53 -0800

    prepare for intial distribution

--- app/js/main.js
+++ app/js/main.js
@@ -4,13 +4,13 @@ var gui = require('nw.gui');
 var converter = require('json-2-csv');
 var $jsonFolder = $("#jsonFolder");
 var $csvFolder = $("#csvFolder");
-var $startConversionButton = $("#startConversionButton");
+var $beginConversionButton = $("#beginConversionButton");
 var jsonFiles;
-var currentJSON;
 var jsonData = [];
 var resultNumber = 10;
 var readPath;
 var writePath;
+
 
 gui.Window.get().on('close', function() {
   this.close(true);
@@ -20,12 +20,12 @@ function isJson(file) {
   return path.extname(file) == '.json';
 };
 
-$startConversionButton.click(function(event) {
+$beginConversionButton.click(function(event) {
   event.preventDefault();
   readPath = $jsonFolder.val() + '/';
   writePath = $csvFolder.val() + '/';
   if($jsonFolder.val() == "" || $csvFolder.val() == "") {
-    alert('Please ensure proper selection of both read AND write directories');
+    alert('Please ensure proper selection of both "read" AND "write" directories');
     return;
   };
   startConversion();
@@ -44,6 +44,11 @@ function readConvertWrite(element, index, array) {
   converter.json2csv(jsonData, function(err, csv) {
     if (err) throw err;
     fs.writeFileSync(writePath + element + '.csv', csv);
+    $beginConversionButton.text('Processed ' + (index+1) + ' out of ' + array.length + ' files');
+    $beginConversionButton.css({
+      'background-color': '#003f88',
+      color: 'white'
+    });
   });
 };
 
